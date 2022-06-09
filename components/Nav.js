60ada58b098d98469ec96646dcde534333d3d28d
Ronald Alfaro <ronald.alfaro91@gmail.com>
2022-06-09 16:43:08 -0600

    Global styles added

--- components/Nav.js
+++ components/Nav.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <nav>
-      <Link href="">Products</Link>
-      <Link href="">Sell</Link>
-      <Link href="">Orders</Link>
-      <Link href="">Accoutn</Link>
+      <Link href="/">Products</Link>
+      <Link href="/">Sell</Link>
+      <Link href="/">Orders</Link>
+      <Link href="/">Accoutn</Link>
     </nav>
   );
 }

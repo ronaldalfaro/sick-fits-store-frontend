c57c5d10d8e89475dd320f11f1b24993feaa2a6e
Ronald Alfaro <ronald.alfaro91@gmail.com>
2022-06-21 12:26:18 -0600

    Apollo setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,40 @@
 import NProgress from 'nprogress';
+import withApollo from 'next-with-apollo';
+import { any } from 'prop-types';
 import Router from 'next/router';
+import { ApolloClient, ApolloProvider } from '@apollo/client';
 import Page from '../components/Page';
-
 import 'nprogress/nprogress.css';
 import '../components/styles/nprogress.css';
+import withData from '../lib/withData';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
 Router.events.on('routeChangeError', () => NProgress.done());
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
+function MyApp({ Component, pageProps, apollo }) {
   return (
-    <Page>
-      <Component {...pageProps} />
-    </Page>
+    <ApolloProvider client={apollo}>
+      <Page>
+        <Component {...pageProps} />
+      </Page>
+    </ApolloProvider>
   );
 }
+
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return pageProps;
+};
+
+MyApp.propTypes = {
+  Component: any,
+  pageProps: any,
+  apollo: ApolloClient,
+};
+
+export default withData(MyApp);
